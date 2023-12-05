49753e15a32624b190c34a2b6183940f1ba35876
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2023-12-04 18:35:09 -0800

    Simplify bodies down a lot

--- src/routes/github_webhook.rs
+++ src/routes/github_webhook.rs
@@ -29,14 +29,32 @@ pub(self) fn verify_signature(secret: &str, body: &[u8], signature: &str) -> Res
     }
 }
 
-pub(self) fn strip_annoying_markdown(input: &str) -> String {
+pub(self) fn simplify_body(input: &str) -> String {
     let comments_regex = regex::Regex::new(r"(?s)<!--(.*?)-->").unwrap();
     let mut output = comments_regex.replace_all(input, "").to_string();
+
+    let reproduction_regex = regex::Regex::new(r"(?sm)## Reproduction:.*?(^.+)").unwrap();
+    if let Some(captures) = reproduction_regex.captures(&output) {
+        output = captures.get(1).unwrap().as_str().to_string();
+    }
 
     let headers_regex = regex::Regex::new(r"(?m)^#+\s*(.*?)\s*$").unwrap();
     output = headers_regex.replace_all(&output, "**$1**").to_string();
 
-    output
+    let images_regex = regex::Regex::new(r"(?s)!\[.*?\]\(.*?\)").unwrap();
+    output = images_regex.replace_all(&output, "").to_string();
+
+    let mut lines = output.lines().collect::<Vec<_>>();
+    lines.retain(|x| !x.is_empty());
+
+    if lines.len() > 4 {
+        lines.truncate(4);
+        lines.push("...");
+    }
+
+    output = lines.join("\n");
+
+    output.trim().to_owned()
 }
 
 #[tracing::instrument(skip(body))]
@@ -74,7 +92,7 @@ pub async fn github_webhook(
             tracing::debug!("received new issue");
 
             let mut body =
-                strip_annoying_markdown(webhook_body["issue"]["body"].as_str().unwrap_or_default());
+                simplify_body(webhook_body["issue"]["body"].as_str().unwrap_or_default());
 
             if body.len() > 500 {
                 body.truncate(500);
@@ -145,26 +163,61 @@ mod tests {
     }
 
     #[test]
-    fn strip_annoying_markdown_headers() {
-        assert_eq!(
-            strip_annoying_markdown("# Header\nText"),
-            "**Header**\nText"
-        )
+    fn simplify_body_headers() {
+        assert_eq!(simplify_body("# Header\nText"), "**Header**\nText")
     }
 
     #[test]
-    fn strip_annoying_markdown_keep() {
+    fn simplify_body_keep() {
         assert_eq!(
-            strip_annoying_markdown("**bold** *italics* __underline__ **__bold and underline__**"),
+            simplify_body("**bold** *italics* __underline__ **__bold and underline__**"),
             "**bold** *italics* __underline__ **__bold and underline__**"
         )
     }
 
     #[test]
-    fn strip_annoying_markdown_comments() {
-        assert_eq!(
-            strip_annoying_markdown("<!-- comment\nwith new lines -->"),
-            ""
-        )
+    fn simplify_body_comments() {
+        assert_eq!(simplify_body("<!-- comment\nwith new lines -->"), "")
+    }
+
+    #[test]
+    fn simplify_images() {
+        assert_eq!(simplify_body("![image](https://google.com)"), "");
+    }
+
+    #[test]
+    fn simplify_template() {
+        let simplified_template = simplify_body(indoc::indoc! {"
+        Reporting client version: 515.1620
+
+        <!-- Write **BELOW** The Headers and **ABOVE** The comments else it may not be viewable -->
+        ## Round ID:
+        [219951](https://scrubby.melonmesa.com/round/219951)
+        <!--- **INCLUDE THE ROUND ID**
+        If you discovered this issue from playing tgstation hosted servers:
+        [Round ID]: # (It can be found in the Status panel or retrieved from https://sb.atlantaned.space/rounds ! The round id let's us look up valuable information and logs for the round the bug happened.)-->
+        
+        <!-- If you are reporting an issue found in another branch or codebase, you MUST link the branch or codebase repo in your issue report or it will be closed. For branches, If you have not pushed your code up, you must either reproduce it on master or push your code up before making an issue report. For other codebases, if you do not have a public code repo you will be refused help unless you can completely reproduce the issue on our code. -->
+        
+        ## Testmerges:
+        - [Attack chain refactoring: Broadening `tool_act` into `item_interact`, moving some item interactions to... `atom/item_interact` / `item/interact_with_atom`](https://www.github.com/tgstation/tgstation/pull/79968)
+        - [Changes Virology Rather Than Killing It](https://www.github.com/tgstation/tgstation/pull/79854)
+        <!-- If you're certain the issue is to be caused by a test merge [OOC tab -> Show Server Revision], report it in the pull request's comment section rather than on the tracker(If you're unsure you can refer to the issue number by prefixing said number with #. The issue number can be found beside the title after submitting it to the tracker).If no testmerges are active, feel free to remove this section. -->
+        
+        ## Reproduction:
+        ![image](https://github.com/tgstation/tgstation/assets/76874615/b4a0d0a0-9ae7-4b46-a9a0-84478b3d980c)
+        By rightclicking an egg into the soup pot, it both drops its reagents into the pot And gets added to the pot as an ingredient
+        you can pull the egg out but it just vanishes
+        <!-- Explain your issue in detail, including the steps to reproduce it. Issues without proper reproduction steps or explanation are open to being ignored/closed by maintainers.-->
+        
+        <!-- **For Admins:** Oddities induced by var-edits and other admin tools are not necessarily bugs. Verify that your issues occur under regular circumstances before reporting them. -->
+        
+        <!-- If you are reporting a runtime error you must include the runtime in your report or your report will be closed. -->
+        "});
+
+        assert!(
+            simplified_template.starts_with("By rightclicking"),
+            "Template doesn't start at reproduction:\n{simplified_template}",
+        );
     }
 }
